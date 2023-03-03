9fe576d28baa50c7f09500cbc41f9838a4dd7188
Vidyaa <mailvidyaa@gmail.com>
2023-03-03 18:22:05 +0000

    Implemented Clouwatch logs

--- backend-flask/services/home_activities.py
+++ backend-flask/services/home_activities.py
@@ -1,10 +1,11 @@
 from datetime import datetime, timedelta, timezone
 from opentelemetry import trace
-
+import logging
 tracer = trace.get_tracer("home.activities")
 
 class HomeActivities:
-  def run():
+  def run(logger):
+    # logger.info("home-activities")
     with tracer.start_as_current_span("home-activities-mock-data"):
       span  = trace.get_current_span()
       now = datetime.now(timezone.utc).astimezone()
