c91a7af4d36ffe5bf91f61078ab1479a5c4b33af
Vidyaa <mailvidyaa@gmail.com>
2023-03-03 17:51:47 +0000

    added spans and traces - code fixed

--- backend-flask/services/home_activities.py
+++ backend-flask/services/home_activities.py
@@ -48,5 +48,5 @@ class HomeActivities:
         'replies': []
       }
       ]
-      span.set_attribute("app.result_length", length(results))
+      span.set_attribute("app.result_length", len(results))
     return results
